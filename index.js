905c2199e20af5f576b1515628f4343a4bf19dc2
Jessica Gladstone <jcg321@comcast.net>
2019-04-28 22:56:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ const ageIsValid = Number.isInteger(currentAge);
 
 const randomNumber = Math.floor(Math.random() *20) +1;
 
-const randomInteger = Math.floor(Math.random()).Number.isInteger();
+const randomInteger = Math.floor(randomNumber);
